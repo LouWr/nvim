6de361dc9f0560f3fe429b3dfabad172fe4a7605
LouWr <louis--wright@hotmail.co.uk>
2025-08-07 17:08:38 +0100

    changes

--- lua/louis/plugins/which-key.lua
+++ lua/louis/plugins/which-key.lua
@@ -1,7 +1,10 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	opts_extend = { "spec" },
 	opts = {
+		preset = "helix",
+		defaults = {},
 		spec = {
 			{
 				mode = { "n", "v" },
@@ -12,6 +15,24 @@ return {
 				{ "<leader>b", group = "Buffers" },
 				{ "<leader>h", group = "Harpoon" },
 				{ "<leader>n", group = "Notifications" },
+
+				{
+					"<leader>b",
+					group = "buffer",
+					expand = function()
+						return require("which-key.extras").expand.buf()
+					end,
+				},
+				{
+					"<leader>w",
+					group = "windows",
+					proxy = "<c-w>",
+					expand = function()
+						return require("which-key.extras").expand.win()
+					end,
+				},
+				-- better descriptions
+				{ "gx", desc = "Open with system app" },
 			},
 		},
 	},
@@ -21,7 +42,22 @@ return {
 			function()
 				require("which-key").show({ global = false })
 			end,
-			desc = "Buffer Local Keymaps (which-key)",
+			desc = "Buffer Keymaps (which-key)",
+		},
+		{
+			"<c-w><space>",
+			function()
+				require("which-key").show({ keys = "<c-w>", loop = true })
+			end,
+			desc = "Window Hydra Mode (which-key)",
 		},
 	},
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		if not vim.tbl_isempty(opts.defaults) then
+			-- LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+			wk.register(opts.defaults)
+		end
+	end,
 }
